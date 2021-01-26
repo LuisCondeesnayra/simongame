d70e6e52357af947aacd2eda7a9083d263cc558b
Luis Conde <76082175+LuisCondeesnayra@users.noreply.github.com>
2021-01-26 02:20:19 -0700

    tap update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ $(document).keypress(function(event) {
     }, 1400);
   }
 });
-$(document).tap(function(event) {
+$(document).on("tap",function(event) {
   if ( nivel == 0) {
     $("#title").html("GO!").addClass("countdown").fadeOut(500).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100);
     $("#level").hide();
